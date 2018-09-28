11944c33099673f5ef7deeb361ab2c3f03cbf035
David Gomez <davidgomezsalamanca@gmail.com>
2018-09-28 16:34:39 -0500

    Clase ruby, conceptos

---
+++ main.rb
@@ -0,0 +1,132 @@
+#nombre_usuario = "Juan"
+
+
+#puts nombre_usuario
+
+#edad = 26
+
+#puts edad+5
+
+#puts 10.to_f/3.to_f
+
+#puts "Hola" + " " + nombre_usuario
+
+#puts "Hola #{nombre_usuario}, sean bienvenidos todos a \"un mundo real\""
+
+#puts 1.methods
+
+#puts "Ingresa un número"
+#numero_uno = gets.chomp.to_f
+#puts "Ingresa otro número"
+#numero_dos = gets.chomp.to_f
+
+#puts "El numero ingresado es #{numero_uno}"
+
+#puts "La suma de estos numero es #{numero_uno+numero_dos}"
+
+#if numero_uno>numero_dos
+#	puts "El numero uno es mayor que numero dos"
+
+#elsif numero_uno==numero_dos
+#	puts"Los numeros son iguales"
+
+#else
+#	puts "El numero uno no es mayor que el numero dos"
+
+#end
+
+#puts "Ingresa tu calificacion:"
+
+
+
+#case Calificacion
+
+#when 5
+	#puts"Excelente"
+#when 4
+	#puts"Sobresaliente"
+#when 3
+    #puts"Aceptable"
+#when 2
+    #puts "Deficiente"
+#when 1
+	#puts "Insuficiente"
+
+#end
+
+
+
+#frutas = ['Manzana','Pera','Uva','Anon','Durazno']
+
+#puts frutas[1]
+
+#frutas.each do |fruta|
+	
+#end
+
+#puts frutas.sort.join(',')
+#puts frutas.include?('Durazno')
+
+#(1..5).each do |numero|
+   #puts "El numero vale #{numero}"
+
+#end
+
+
+#i=0
+#while i<10
+  #puts "I vale #{i}"
+  #i=i+1
+#end
+
+#metodo include 
+#while
+
+#begin
+	#i=i+1
+	#puts"i vale #{i}"
+    #i=i+1
+#end while i<10
+	
+#end
+
+#5.times do
+   #puts"Hola"
+#end
+
+#5.upto(10) do |valor|
+  #puts valor
+
+#end
+
+#10.downto(5) do |valor|
+   #puts valor 
+#end
+ 
+ #hashes es algo muy parecido a los objetos 
+
+persona = { "nombre" => "Juan", "edad" => 26 }
+
+puts persona["edad"]
+
+persona.each do |clave, valor|
+	puts "La clave es #{clave} y el valor es #{valor}"
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
